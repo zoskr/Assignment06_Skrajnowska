8d205715631fc47707e78298ef55c0fbea890e22
zoskr <skrajnowskaz@gmail.com>
2024-03-18 14:33:41 -0400

    Question 1 Completed

--- Assignment06_Question01_Skrajnowska.py
+++ Assignment06_Question01_Skrajnowska.py
@@ -1,7 +1,52 @@
-class Animal():
+class Animal:
     def __init__(self, name):
         self.name = name
-    def eat():
-        print("Animal is eating.")
-    def sleep():
-        print("Animal is sleeping")
+    def eat(self):
+        print(self.name, "is eating.")
+    def sleep(self):
+        print(self.name, "is sleeping")
+        
+class Mammal(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+        
+class Bird(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+    def sleep(self):
+        print(self.name, "sleeps in a nest")
+
+class Fish(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+        
+class Cat(Mammal):
+    def __init__(self, name):
+        super().__init__(name)
+    def eat(self):
+        print(self.name, "eats cat food.")
+
+class Dog(Mammal):
+    def __init__(self, name):
+        super().__init__(name)
+    def eat(self):
+        print(self.name, "eats dog food")
+        
+class Eagle(Bird):
+    def __init__(self, name):
+        super().__init__(name)
+    def eat(self):
+        print(self.name, "eats eagle food.")
+
+
+def main():
+    bob = Dog("Bob")
+    bob.eat()
+    bob.sleep()
+    
+    steve = Eagle("Steve")
+    steve.eat()
+    steve.sleep()
+    
+main()
+        
